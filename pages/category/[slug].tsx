52a99850ceb57939fdcf4bb4caa616aafc4778da
Pranay Yadav <pranayyadav08@gmail.com>
2021-11-11 14:20:32 +0530

    Added Category Post page and Comments

--- pages/category/[slug].tsx
+++ pages/category/[slug].tsx
@@ -30,7 +30,6 @@ const CategoryPost = ({ posts }) => {
 };
 export default CategoryPost;
 
-// Fetch data at build time
 export async function getStaticProps({ params }) {
   const posts = await getCategoryPost(params.slug);
 
@@ -39,8 +38,6 @@ export async function getStaticProps({ params }) {
   };
 }
 
-// Specify dynamic routes to pre-render pages based on data.
-// The HTML is generated at build time and will be reused on each request.
 export async function getStaticPaths() {
   const categories = await getCategories();
   return {
