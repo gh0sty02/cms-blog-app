ea0eeaeeffc87a0a49da5a3cb3d4da9385a68815
Pranay Yadav <pranayyadav08@gmail.com>
2021-11-11 13:31:24 +0530

    Added Category Post page and Comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -23,16 +23,18 @@ const PostDetails: React.FC<IPost> = ({ post }) => {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
           {<PostDetail post={post} />}
-          <Author author={post.author} />
-          <CommentsForm slug={post.slug} />
-          <Comments slug={post.slug} />
+          {post.author && <Author author={post.author} />}
+          {post.slug && <CommentsForm slug={post.slug} />}
+          {post.slug && <Comments slug={post.slug} />}
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative lg:sticky top-8">
-            <PostWidget
-              slug={post.slug}
-              categories={post.categories.map((category) => category.slug)}
-            />
+            {post.slug && (
+              <PostWidget
+                slug={post.slug}
+                categories={post.categories.map((category) => category.slug)}
+              />
+            )}
 
             <Categories />
           </div>
